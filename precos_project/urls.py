7b9344f73188b18d38f1a1dd81901658f614b421
Gabriel Moraes <gab.gam123@gmail.com>
2025-07-18 10:28:22 -0400

    18/07/2025 configurações de usuarios

--- precos_project/urls.py
+++ precos_project/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     # Inclui as URLs do app calculator na raiz do site
     path('', include('calculator.urls')),
     # Inclui as URLs de autenticação (login, logout, etc.)
-    path('contas/', include('django.contrib.auth.urls')),
+    path('/contas/login/', include('django.contrib.auth.urls')),
 ]
 LOGIN_REDIRECT_URL = 'dashboard'
 LOGOUT_REDIRECT_URL = 'login'
